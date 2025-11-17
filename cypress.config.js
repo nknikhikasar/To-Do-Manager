6f9b31779144fd87f10f9860c1fc699f37c16535
nknikhikasar <nkasar68@gmail.com>
2025-11-17 23:06:07 +0530

    Add environment variable setup for CI/CD workflows and update Cypress base URL configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.BASE_URL || "http://localhost:5173",
   },
   component: {
     devServer: {
