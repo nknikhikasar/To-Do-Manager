5c35c7a8b8a6d00cd966bb718dac5e0493e3eb40
nknikhikasar <nkasar68@gmail.com>
2025-11-17 20:21:23 +0530

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/To-Do-Manager/', 
   plugins: [
     vue(),
     vueDevTools(),
