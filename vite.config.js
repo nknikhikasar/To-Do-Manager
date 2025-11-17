ca09dac7a1d9c14ed35fcde3afdb4bd7a0fc4d10
nknikhikasar <nkasar68@gmail.com>
2025-11-17 22:25:06 +0530

    Remove base path configuration from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/To-Do-Manager/', 
   plugins: [
     vue(),
     vueDevTools(),
