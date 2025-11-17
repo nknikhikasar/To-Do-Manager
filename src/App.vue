8140f54633cd19be3c9a42f8a0b35ca87aa0761a
nknikhikasar <nkasar68@gmail.com>
2025-11-17 23:51:57 +0530

    Update routing paths in App.vue and router.js to include 'dev' subdirectory

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/To-Do-Manager/">Home</router-link> |
-    <router-link to="/To-Do-Manager/about">About</router-link>
+    <router-link to="/To-Do-Manager/dev/">Home</router-link> |
+    <router-link to="/To-Do-Manager/dev/about">About</router-link>
   </nav>
   <router-view />
 </template>
