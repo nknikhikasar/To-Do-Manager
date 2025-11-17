622c58be146618e36116773465fbbb6ac9336758
nknikhikasar <nkasar68@gmail.com>
2025-11-17 23:48:31 +0530

    Update router paths to include 'stage' for improved routing structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/To-Do-Manager/">Home</router-link> |
-    <router-link to="/To-Do-Manager/about">About</router-link>
+    <router-link to="/To-Do-Manager/stage/">Home</router-link> |
+    <router-link to="/To-Do-Manager/stage/about">About</router-link>
   </nav>
   <router-view />
 </template>
